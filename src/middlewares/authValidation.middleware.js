1e44e9db818e4d2b2c683c51e79d436580550778
matheussvini <matheussvini@outlook.com>
2022-11-25 00:29:21 -0300

    test: auth validation

--- src/middlewares/authValidation.middleware.js
+++ src/middlewares/authValidation.middleware.js
@@ -1,6 +1,35 @@
+import jwt from "jsonwebtoken";
+import { usersCollection } from "../database/db.js";
+import dotenv from "dotenv";
+dotenv.config();
+
 export async function authValidation(req, res, next) {
+  const { authorization } = req.headers;
+  const token = authorization?.replace("Bearer ", "");
+  const secretKey = process.env.JWT_SECRET;
 
-    // validação para transações do usuário (verifica token) 
+  if (!token) {
+    return res.status(401).send({
+      message:
+        'Envie um header na requisição com campo "authorization" com valor "Bearer TokenDoUsuario"!',
+    });
+  }
 
-    next()
+  try {
+    const data = jwt.verify(token, secretKey);
+
+    const user = await usersCollection.findOne({ _id: data?.userId });
+
+    if (!user) {
+      return res
+        .status(401)
+        .send({ message: "Token inválido, faça login novamente." });
+    }
+
+    delete user.password;
+    res.locals.user = user;
+  } catch (err) {
+    res.status(500).send(err.message);
+  }
+  next();
 }
